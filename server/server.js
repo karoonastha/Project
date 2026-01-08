fdddad76fd58f2bf6d264bd5685fa6af9a141da2
Karuna Shrestha <karoonasth@gmail.com>
2026-01-08 16:01:09 +0545

    chore: configure gitignore and untrack node_modules and .env

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js"
+import { clerkMiddleware } from '@clerk/express'
+
 
 await connectDB() 
 
 
 const app = express();
 app.use(cors());
+
+//Middleware Setup
+app.use(express.json()) //Enables JSOM request body parsing
+app.use(clerkMiddleware())
+
 
 app.get('/', (req, res) => {
     res.send("API successfully connected.");
